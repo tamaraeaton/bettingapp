2e138db73bb63b8f29b3264322b813d1db70b3f2
Adrian Diaz <adiaz@banyanlabs.io>
2021-08-10 11:24:54 -0700

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <p>lorem...</p>
+      </div>
+      <div className="footer-right">
+        <a href="#" className="footer-link">
+          Contact Us
+        </a>
+      </div>
+    </div>
+  );
+};
