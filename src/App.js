79e7d7767c15294d46432bc6c740484ea50599f7
Adrian Diaz <adiaz@banyanlabs.io>
2021-08-11 12:49:49 -0700

    basic cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Members from "./components/Members/Members";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Members />
     </div>
   );
 }
---
+++ src/components/Members/Members.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./members.css";
+
+const Members = () => {
+  return (
+    <div className="members">
+      <div className="members-card">
+        <img
+          className="members-img"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTku2jdlcVmdWapW-jyVJq33TH893-rB36kkw&usqp=CAU"
+          alt="this is adrian"
+        />
+        <h1 className="members-name">Adrian Diaz</h1>
+        <p>Project Member</p>
+      </div>
+      <div className="members-card">
+        <img
+          className="members-img"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTku2jdlcVmdWapW-jyVJq33TH893-rB36kkw&usqp=CAU"
+          alt="this is jason"
+        />
+        <h1 className="members-name">Jason Lyles</h1>
+        <p>Project Member</p>
+      </div>
+    </div>
+  );
+};
+
+export default Members;
