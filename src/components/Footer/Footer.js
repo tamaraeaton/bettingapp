5887e4445d9a9a62405e2e8eca42c404b8dd91e9
Adrian Diaz <adiaz@banyanlabs.io>
2021-08-12 07:40:21 -0700

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,6 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-left">
-        <p>lorem...</p>
-      </div>
       <div className="footer-right">
         <a href="#" className="footer-link">
           Contact Us
