dcce2ccb1c6850552593cb94f482eee0d6cf283b
Adrian Diaz <adiaz@banyanlabs.io>
2021-08-18 14:45:32 -0700

    fixed a lot of things
    Please please merge this. this has a lot fixes i made to the code. Like everything even the app js. it would be really bad if i had to add all those fixes again. thank you!

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "./Welcome.css";
 import FriendsBetting from "../../assets/friendsBetting.PNG";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <div>
-      <div className='welcome'>
+      <div className="welcome">
         <div className="links-section">
-        <a className="login-anchor"href="#"><h1 className='login'>Login</h1></a>
-        <a href="#" className="signup-anchor"><h1 className='sign-up'>
-          Sign up
-        </h1></a>
+          <Link to="/login" className="login-anchor">
+            <h1 className="login">Login</h1>
+          </Link>
+          <Link to="/register" className="signup-anchor">
+            <h1 className="sign-up">Sign Up</h1>
+          </Link>
         </div>
-        <img id='friends-betting-img' src={FriendsBetting} alt="bettingFriends" />
+        <img
+          id="friends-betting-img"
+          src={FriendsBetting}
+          alt="bettingFriends"
+        />
       </div>
-      <div className='intro'>
+      <div className="intro">
         <h3 className="intro-text">
-          Have a little fun placing bets with your friends and others with ease on our friendly wager app.
+          Have a little fun placing bets with your friends and others with ease
+          on our friendly wager app.
           <br />
-          You can create your own silly bet with friends or place your bet with current open bets.
+          You can create your own silly bet with friends or place your bet with
+          current open bets.
         </h3>
       </div>
     </div>
