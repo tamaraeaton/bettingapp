81b3f0087e51f4eb7805183239556825428a9985
Adrian Diaz <adiaz@banyanlabs.io>
2021-08-18 14:17:10 -0700

    conditionally renders logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import './Header.css'
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        
-        <div className='calheader'>
-            <div className='company-section'>
-                <div className='twire-logo'></div>
-                <div><a href=""  className='twire-link'><link src="url"/>TWIRE</a></div>
-            </div>
-            <div className='header-link-section'>
-            <div><a href=""  className='log-out'><link src="url"/>Log Out</a></div>
-                <a href=""  className='about-link'><link src="url"/>About</a>
-            </div>
-           
+  const [isAuth, setIsAuth] = useState(true);
+  console.log(isAuth)
+  return (
+    <div className="calheader">
+      <div className="company-section">
+        <div className="twire-logo"></div>
+        <div>
+          <a href="" className="twire-link">
+            <link src="url" />
+            TWIRE
+          </a>
         </div>
-    
-    )
-}
+      </div>
+      <div className="header-link-section">
+        <div>
+          {isAuth ? (
+            <Link to="/" onClick={() => setIsAuth(false)} className="log-out">
+              <link src="url" />
+              Log Out
+            </Link>
+          ) : null}
+        </div>
+        <a href="" className="about-link">
+          <link src="url" />
+          About
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
