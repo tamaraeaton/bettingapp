0ea4cf52b51db656ee0452071732c1fca211f80d
tamaraeaton <80291920+tamaraeaton@users.noreply.github.com>
2021-08-18 08:30:21 -0700

    Create a Bet Router added
    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 import Header from "./components/Header/Header.js";
 import Welcome from "./components/Welcome/Welcome";
 import Footer from "./components/Footer/Footer";
-import Home from './components/Home/Home'
-
+import Home from "./components/Home/Home";
+import BetFrom from "./components/BetFrom/BetForm"
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Welcome />
-      <Footer />
-      {/* <Home/> */}
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <Welcome />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/bet-form" component={BetFrom} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
