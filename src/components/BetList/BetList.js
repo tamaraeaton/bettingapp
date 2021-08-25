1c6ec91824c270f7ee8165c104ef254772216ae1
tamaraeaton <80291920+tamaraeaton@users.noreply.github.com>
2021-08-25 08:20:49 -0700

    Added Table

--- src/components/BetList/BetList.js
+++ src/components/BetList/BetList.js
@@ -4,24 +4,32 @@ import "./BetList.css";
 const BetList = ({ bets }) => {
   return (
     <div>
-      <h2 className="bet-list-title">Bet List</h2>
+      <h2 className='bet-list-title'>Bet List</h2>
 
-      <div id="bet-list">
+      <div id='bet-list'>
         <table
-          id="table-list"
-          className="table table-striped"
+          id='table-list'
+          className='table table-striped'
           style={{ marginTop: 20 }}
         >
-          {bets.map((bet) => (
-            <thead key={bet.id}>
-              <tr className="bet-list-bet">
-                <th>Bet Name: {bet.name}</th>
-                <th>Amount: {bet.amount}</th>
-                <th>Description: {bet.description}</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-          ))}
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody id="bet-items">
+            {bets.map((bet)=>(
+                <tr>
+                    <td>{bet.name}</td>
+                    <td>{bet.amount}</td>
+                    <td><button>Join Bet</button></td>
+                </tr>
+            ))
+
+            }
+          </tbody>
         </table>
       </div>
     </div>
