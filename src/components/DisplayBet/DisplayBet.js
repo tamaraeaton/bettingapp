507d1bab4183b06557a8e9584f10ca0d663701de
JATlyles <jlyles@banyanlabs.io>
2021-09-01 12:11:49 -0500

    updated

--- src/components/DisplayBet/DisplayBet.js
+++ src/components/DisplayBet/DisplayBet.js
@@ -4,28 +4,37 @@ import { AuthContext } from "../../context/Auth";
 import { AppContext } from "../../context/AppContext";
 
 const DisplayBet = ({ bet }) => {
-  const { currentUser } = useContext(AuthContext);
-  const { disBet } = useContext(AppContext);
-  const currentUserId = currentUser ? currentUser.uid : null;
-  return (
-    <div className="join-bet">
-      <div>
-        <h1 className="name">Bet Name: {disBet.name}</h1>
-      </div>
-      <div>
-        <h1 className="bd">Bet Description: {disBet.description}</h1>
-      </div>
-      <div>
-        <h1 className="Am">Amount:</h1>
-      </div>
-      {currentUserId === disBet.owner ? "Members" : null}
-      {currentUserId === disBet.owner ? (
-        <button>See Members</button>
-      ) : (
-        <button>Join Bet</button>
-      )}
-    </div>
-  );
+    const { currentUser } = useContext(AuthContext);
+    const { disBet } = useContext(AppContext);
+    console.log(disBet)
+    console.log(currentUser)
+    const currentUserId = currentUser ? currentUser.uid : null;
+    return (
+        <div className="join-bet">
+            <div>
+                <h1 className="name">Bet Name: {disBet.name}</h1>
+            </div>
+            <div>
+                {currentUserId === disBet.owner ? <h1 className="Oh">Owner/Host: You</h1> : <h1 className="Oh">Owner/Host: {disBet.ownerhost}</h1>}
+            </div>
+            <div>
+                <h1 className="bd">Bet Description: {disBet.description}</h1>
+            </div>
+            <div>
+                <h1 className="bd">Members: Number Of Members</h1>
+            </div>
+            <div>
+                <h1 className="Am">Total Amount:</h1>
+            </div>
+            {currentUserId === disBet.owner ? (
+                <button>See Members</button>
+            ) : (
+                <button>Join Bet</button>
+            )}
+        </div>
+
+    );
 };
+
 
 export default DisplayBet;
