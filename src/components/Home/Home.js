b83ace397410a49a745ba885a59358aef58637c1
tamaraeaton <teaton@banyanlabs.io>
2021-09-14 11:04:22 -0700

    CL-68 Added Welcome, user

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Home.css";
 import { Link, withRouter } from "react-router-dom";
 import BetList from "../BetList/BetList";
+import { AuthContext } from "../../context/Auth";
 
 const Home = () => {
+  const { currentUser } = useContext(AuthContext);
+  
   return (
-    <div className="home general flex-component">
-      <div className="create-bet-here">
-        <h4 className="bet-intro">Create a bet with your friends</h4>
-        <button className="custom-button">
-          <Link to="/bet-form">Create Bet</Link>
-        </button>
+    <div className='home general flex-component'>
+      <div className="welcome-user-container">
+        <h1>Welcome, {currentUser.firstName}</h1>
+        <div className='create-bet-here'>
+          <h4 className='bet-intro'>Create a bet with your friends</h4>
+          <button className='custom-button'>
+            <Link to='/bet-form'>Create Bet</Link>
+          </button>
+        </div>
       </div>
       <BetList />
     </div>
