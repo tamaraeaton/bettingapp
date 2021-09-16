6300a580c3c6f91d98098a687f1d41831c5b5cc5
tamaraeaton <teaton@banyanlabs.io>
2021-09-16 14:17:46 -0700

    fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,12 +12,13 @@ const Home = () => {
   return (
     <div className="home general flex-component">
         <div className="welcome-user-container">
-        <h1>Welcome, {currentUser.firstName}</h1>
+        {/* <h1>Welcome, {currentUser.firstName}</h1> */}
       <div className="create-bet-here">
         <h4 className="bet-intro">Create a bet with your friends</h4>
         <ButtonText link="/bet-form" text="Create Bet"/>
       </div>
       <BetList />
+    </div>
     </div>
   );
 };
