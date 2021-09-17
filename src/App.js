f1a4e6c139e3b39343e59a47eb6bdbeb94ac5bc0
tamaraeaton <teaton@banyanlabs.io>
2021-09-17 08:42:41 -0700

    Fixed Welcome CSS and Banner on Home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/Header/Header.js";
--- src/components/BetList/BetList.js
+++ src/components/BetList/BetList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import "./BetList.css";
 import { AuthContext } from "../../context/Auth";
 import { AppContext } from "../../context/AppContext";
