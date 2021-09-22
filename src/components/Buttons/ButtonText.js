c7faf377cf410d060f1348caec68e462198f13e4
tamaraeaton <teaton@banyanlabs.io>
2021-09-22 11:33:45 -0700

    Made Create Bet Button Bigger

--- src/components/Buttons/ButtonText.js
+++ src/components/Buttons/ButtonText.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ButtonText = ({ link, text, betToggle }) => {
+const ButtonText = ({ link, text, betToggle, createButton }) => {
   return (
     <div>
-      {betToggle ? <button className="custom-button" onClick={betToggle}>
-        <Link to={link}>{text}</Link >
-      </button > : <button className="custom-button">
-        <Link to={link}>{text}</Link>
-      </button>}
+      {betToggle ? (
+        <button className='custom-button' onClick={betToggle}>
+          <Link to={link}>{text}</Link>
+        </button>
+      ) : createButton ? (
+        <button className='custom-button' style={createButton}>Create Bet</button>
+      ) : (
+        <button className='custom-button'>
+          <Link to={link}>{text}</Link>
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default ButtonText;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,15 +7,20 @@ import { AuthContext } from "../../context/Auth";
 
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
+  
+  const createButton = {
+    fontSize: 30,
+    width: 500,
+    height: 80,
+  } 
 
   return (
     <div className='home general flex-component'>
-
       <div className='welcome-user-container'>
-      <h1 className="welcome-name">Welcome, {currentUser.firstName}</h1>
-        <ButtonText className="create-button" link='/bet-form' text='Create Bet' />
+        <h1 className='welcome-name'>Welcome, {currentUser.firstName}</h1>
         <div className='create-bet-here'>
           <h4 className='bet-intro'>Create a bet with your friends</h4>
+          <ButtonText createButton={createButton} link='/bet-form' text='Create Bet' />
         </div>
       </div>
       <BetList />
