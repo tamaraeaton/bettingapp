f184f0a079be52cc2c67b283e3b272de13761bfb
Adrian Diaz <adiaz@banyanlabs.io>
2021-09-29 10:37:46 -0700

    CL-99-AD
    fixed the date picker on the register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -12,6 +12,7 @@ const Register = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [age, setAge] = useState("");
+  const [dob, setDob] = useState("");
   const [toggleNotification, setToggleNotification] = useState(false);
   const [gender, setGender] = useState("");
   const [toggle, setToggle] = useState(false);
@@ -24,155 +25,141 @@ const Register = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (age < 18) return;
-
-    const newUser = {
-      firstName,
-      lastName,
-      email,
-      age,
-      gender,
-    };
- 
-
-    if (confirmPassword !== password) {
-      setErrMsg("Passwords do not match!");
-      setPassword("");
-      setConfirmPassword("");
+    if (age < 21) {
+      setErrMsg("Must be at least 21 years of age!");
     } else {
-      register(newUser, password)
-        .then((user) => {
-          addUser(newUser, user.user.uid).then(() => {
-            setEmail("");
-            setPassword("");
-            setConfirmPassword("");
-            setErrMsg("");
-            history.push("/home");
-          });
-        })
-        .catch((err) => setErrMsg(err.message));
+      const newUser = {
+        firstName,
+        lastName,
+        email,
+        dob,
+        gender,
+      };
+
+      if (confirmPassword !== password) {
+        setErrMsg("Passwords do not match!");
+        setPassword("");
+        setConfirmPassword("");
+      } else {
+        register(newUser, password)
+          .then((user) => {
+            addUser(newUser, user.user.uid).then(() => {
+              setEmail("");
+              setPassword("");
+              setConfirmPassword("");
+              setErrMsg("");
+              history.push("/home");
+            });
+          })
+          .catch((err) => setErrMsg(err.message));
+      }
     }
   };
-
-  const calAge = (date) =>
-    new Date(Date.now() - new Date(date).getTime()).getFullYear() - 1970;
 
   const ageValidator = (e) => {
     setAge(calAge(e.target.value));
-    if (calAge(e.target.value) < 18) {
-      setToggleNotification(true);
-    }
+    setDob(e.target.value);
   };
-
-  console.log(gender);
+  
+  const calAge = (date) =>
+    new Date(Date.now() - new Date(date).getTime()).getFullYear() - 1970;
+  console.log(age);
 
   return (
-    <div className='general flex-component custom-form-page'>
-      {errMsg ? <div className='error'>{errMsg}</div> : null}
-      <h2 className='custom-form-title'>Sign Up</h2>
-      <form className='custom-form' onSubmit={handleSubmit}>
+    <div className="general flex-component custom-form-page">
+      <h2 className="custom-form-title">Sign Up</h2>
+      {errMsg ? <div className="error">{errMsg}</div> : null}
+      <form className="custom-form" onSubmit={handleSubmit}>
         <input
-          type='email'
-          name='email'
-          placeholder='Email'
-          className='custom-input'
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="custom-input"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
         <input
-          type='text'
-          name='firstName'
-          placeholder='First Name'
-          className='custom-input'
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          className="custom-input"
           onChange={(e) => setFirstName(e.target.value)}
           value={firstName}
-          required
         />
         <input
-          type='text'
-          name='lastName'
-          placeholder='Last Name'
-          className='custom-input'
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          className="custom-input"
           onChange={(e) => setLastName(e.target.value)}
           value={lastName}
         />
         <input
-          type='date'
-          name='dob'
-          className='custom-input'
+          type="date"
+          name="dob"
+          className="custom-input"
           onChange={(e) => ageValidator(e)}
         />
-        {toggleNotification && (
-          <div className='notification-box'>
-            <div className='notification'>You are only {age}, you brat</div>
-            {/* <div
-              className='notification-closer'
-              onClick={() => setToggleNotification(!toggleNotification)}
-            >
-              &#10006;
-            </div> */}
-          </div>
-        )}
-        <div className='custom-radio-wrapper'>
-          <div className='custom-radio'>
+        <div className="custom-radio-wrapper">
+          <div className="custom-radio">
             <p style={{ marginRight: ".3rem" }}>Male </p>
             <input
-              type='radio'
-              value='Male'
-              name='gender'
+              type="radio"
+              value="Male"
+              name="gender"
               onClick={(e) => {
                 setGender("Male");
                 setToggle(false);
               }}
             />
           </div>
-          <div className='custom-radio'>
+          <div className="custom-radio">
             <p style={{ marginRight: ".3rem" }}>Female </p>
             <input
-              type='radio'
-              value='Female'
-              name='gender'
+              type="radio"
+              value="Female"
+              name="gender"
               onClick={(e) => {
                 setGender("Female");
                 setToggle(false);
               }}
             />
           </div>
-          <div className='custom-radio'>
+          <div className="custom-radio">
             <p style={{ marginRight: ".3rem" }}>Other </p>
-            <input type='radio' name='gender' onClick={toggleGenderField} />
+            <input type="radio" name="gender" onClick={toggleGenderField} />
           </div>
         </div>
         {toggle ? (
           <input
-            type='text'
-            name='gender'
-            placeholder='Gender'
-            className='custom-input'
+            type="text"
+            name="gender"
+            placeholder="Gender"
+            className="custom-input"
             onChange={(e) => setGender(e.target.value)}
             value={gender}
           />
         ) : null}
 
         <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          className='custom-input'
+          type="password"
+          name="password"
+          placeholder="Password"
+          className="custom-input"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
         <input
-          type='password'
-          name='confirmPassword'
-          placeholder='Confirm Password'
-          className='custom-input'
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          className="custom-input"
           onChange={(e) => setConfirmPassword(e.target.value)}
           value={confirmPassword}
         />
-        <input type='submit' value='Sign up' className='custom-button' />
+        <input type="submit" value="Sign up" className="custom-button" />
       </form>
-      <Link className='login-link' to='/login'>
+      <Link className="login-link" to="/login">
         If you are already registered, click here to Login.
       </Link>
     </div>
