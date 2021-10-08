ce9a1aec7f08a75766850376561117f97a24157e
tamaraeaton <teaton@banyanlabs.io>
2021-10-08 08:58:11 -0700

    Update Contact Form CSS

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,21 +4,6 @@ import "./ContactForm.css";
 import emailjs from "emailjs-com";
 
 const ContactForm = () => {
-  // const [msg, setMsg] = useState({
-  //   name: "",
-  //   email: "",
-  //   text: "",
-  //   disabled: false,
-  //   emailSent: false,
-  // });
-
-  // const handleChange = (e) => {
-  //   setMsg({
-  //     ...msg,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     emailjs
@@ -37,18 +22,41 @@ const ContactForm = () => {
 
   return (
     <div className='general flex-component custom-form-page'>
-      <h1 className="contact-form-title">Please feel free to contact us with your questions
-        or comments. We will reply within 2-4 days.</h1>
+      <h1 className='contact-form-title'>
+        Please feel free to contact us with your questions or comments.</h1>
       <form onSubmit={handleSubmit} className='contact-form custom-form'>
-        <label>Name</label>
-        <input className="textarea" type='text' name='user_name' required />
-        <label>Email</label>
-        <input className="textarea" type='text' name='user_email' required />
-        <label>Subject</label>
-        <input className="textarea" type="text" name="subject" required />
-        <label>Comment</label>
-        <input className="textarea" type='text' name='text' required />
-        <input className="send-message-button custom-button" type='submit' value='Send Message' />
+        <input
+          className='custom-input'
+          type='text'
+          placeholder='User Name'
+          name='user_name'
+          required
+        />
+        <input
+          className='custom-input'
+          type='text'
+          placeholder='Email'
+          name='email'
+          required
+        />
+        <input
+          className='custom-input'
+          type='text'
+          placeholder='Subject'
+          name='subject'
+          required
+        />
+        <textarea
+          rows="5"
+          placeholder="Comment..."
+          className="comment-textarea"
+          name="text"
+          required/>
+         <input
+          className='send-message-button custom-button'
+          type='submit'
+          value='Send Message'
+        />
       </form>
     </div>
   );
