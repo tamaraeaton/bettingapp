62f1bd45a82ed59da011ec373bc48835fc1b0667
Adrian Diaz <adiaz@banyanlabs.io>
2021-10-12 23:11:39 -0700

    displaying done

--- src/components/DisplayBet/DisplayBet.js
+++ src/components/DisplayBet/DisplayBet.js
@@ -3,39 +3,57 @@ import "./DisplayBet.css";
 import { AuthContext } from "../../context/Auth";
 import { AppContext } from "../../context/AppContext";
 import ButtonText from "../Buttons/ButtonText";
+import { useState } from "react/cjs/react.development";
 
 const DisplayBet = ({ bet }) => {
   const { currentUser } = useContext(AuthContext);
   const { disBet, getBetMembers } = useContext(AppContext);
+  const [theChoices, setTheChoices] = useState(
+    disBet.choices.filter((cho) => (cho.name !== "" ? cho : null))
+  );
 
   useEffect(() => {
     getBetMembers(disBet.id);
   }, []);
 
   const currentUserId = currentUser ? currentUser.uid : null;
+
+  console.log(theChoices);
   return (
-    <div className='join-bet'>
-      <div>
-        <h1 className='name'>Bet Name: {disBet.name}</h1>
+    <div className="general join-bet">
+      <div className="bet-information">
+        <div className="info1">
+          <h2>
+            Bet:{" "}
+            <u>
+              <i>{disBet.name}</i>
+            </u>
+          </h2>
+          <h3>Pot: ${disBet.potTotal}</h3>
+          <h4>{disBet.description}</h4>
+        </div>
+
+        <h5 className="info2">
+          Questions about the bet? Email the creator, <u>{disBet.ownerEmail}</u>
+        </h5>
       </div>
-      <div>
-        {currentUserId === disBet.owner ? (
-          <h1 className='Oh'>Owner/Host: You</h1>
-        ) : (
-          <h1 className='Oh'>Owner/Host: {disBet.ownerEmail}</h1>
-        )}
+      <div className="choices-container">
+        {theChoices.map((cho, index) => (
+          <div key={index} className="a-choice-container">
+            <div className="item-info">
+              <p className="choice-name">{cho.name}</p>
+              <p className="choice-description">-{cho.description}</p>
+            </div>
+            <button className="choice-button">Join</button>
+          </div>
+        ))}
       </div>
-      <div>
-        <h1 className='bd'>Bet Description: {disBet.description}</h1>
+
+      <div className="button-container">
+        <ButtonText link="/display-members" text="Display Members" />
       </div>
-      <div>
-        <h1 className='Am'>Pot Total: $ {disBet.potTotal}</h1>
-      </div>
-      <div className='button-container'>
-        <ButtonText link='/display-members' text='Display Members' />
-      </div>
-      <div className='button-container'>
-        <ButtonText link='/join-bet' text='Join Bet' />
+      <div className="button-container">
+        <ButtonText link="/join-bet" text="Join Bet" />
       </div>
     </div>
   );
